310fd1eca7a3913e0be5b1235fa842f772f80fcb
Teddyburgonde <bandama.teddy@gmail.com>
2026-06-14 00:20:03 +0200

    Added function setup_signal

--- srcs/signal.c
+++ srcs/signal.c
@@ -1,8 +1,19 @@
 #include "ft_malcom.h"
 
-void handler(int sig)
+/*
+** Handler appelé quand l'utilisateur fait Ctrl+C (SIGINT).
+** Met g_running à false pour sortir proprement de la boucle d'écoute.
+*/
+void	handler(int sig)
 {
 	(void)sig;
-
 	g_running = false;
+}
+
+/*
+** Branche le handler sur le signal SIGINT (Ctrl+C).
+*/
+void	setup_signal(void)
+{
+	signal(SIGINT, handler);
 }
